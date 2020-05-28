955db0e96160d86f2f90a50fc8ca9081174668bc
Sumanth Chennupati <sumanth.chennupati@zenuity.com>
2020-05-28 14:57:48 -0400

    Update .gitignore

--- res5_supervision_and_loss_combination_experiments.sh
+++ res5_supervision_and_loss_combination_experiments.sh
@@ -10,10 +10,10 @@ wait
 
 # Multiclass with and without res5 supervision, uncertainty
 
-python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x.yaml --num-gpus 2
-wait
-python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x_uncertainty.yaml --num-gpus 2
-wait
-python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x_res5supervision.yaml --num-gpus 2
-wait
-python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x_uncertainty_res5supervision.yaml --num-gpus 2
+# python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x.yaml --num-gpus 2
+# wait
+# python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x_uncertainty.yaml --num-gpus 2
+# wait
+# python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x_res5supervision.yaml --num-gpus 2
+# wait
+# python train_net.py --config-file configs/cityscapes_panoptic_contour_multi_seperate_R_50_1x_uncertainty_res5supervision.yaml --num-gpus 2
